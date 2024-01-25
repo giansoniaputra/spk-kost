81e39a6c46362a700960f632dd07fb0a41538175
giansoniaputra <giansonia555@gmail.com>
2024-01-25 22:59:58 +0700

    Selesai Tanpa User

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -139,7 +139,7 @@
     <!-- Brand Logo -->
     <a href="/" class="brand-link">
         <img src="/assets/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">SPK KOST</span>
+        <span class="brand-text font-weight-light">SPK</span>
     </a>
 
     <!-- Sidebar -->
